adad8d91f37d70e89098b53f9821324c49d41e76
sshellen <sshellen@users.noreply.github.com>
2020-03-25 18:02:55 -0700

    Update HatDrop.js

--- src/Components/HatDrop.js
+++ src/Components/HatDrop.js
@@ -5,32 +5,72 @@ import ImageContext from "../Components/Context";
 let hatDrop;
 let hatDropLeft = 0;
 let hatDropInterval;
+let hatDropFrame = null;
 let mc;
 let speed = 5;
 let phase = 1;
 let eventFunction;
-let total = 0;
 
 class HatDrop extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { hat: "hat", left: 0, showSettings: false,speed: 5, speedSetting:"gradual" };
+    this.state = {
+      hat: "hat",
+      left: 0,
+      showSettings: false,
+      speed: 5,
+      speedSetting: "gradual"
+    };
   }
 
   moveHatDrop = () => {
-    hatDropInterval = setInterval(() => {
+    hatDropInterval = setTimeout(() => {
       hatDropLeft += speed;
       hatDrop.style.left = hatDropLeft + "px";
       if (hatDropLeft > this.props.rightWall) {
         hatDropLeft = -60;
       }
+      window.clearTimeout(hatDropInterval);
+      hatDropFrame = window.requestAnimationFrame(this.moveHatDrop);
     }, 60);
+  };
+
+  componentDidUpdate(prev, curr) {
+    if (this.props.phase !== phase) {
+      phase = this.props.phase;
+      if (phase === 1) {
+        speed = 8;
+      }
+      if (phase === 2) {
+        speed = 11;
+      }
+      if (phase === 3) {
+        speed = 14;
+      }
+      if (phase === 4) {
+        speed = 17;
+      }
+    }
+
+    if (this.state.showSettings !== this.props.showSettings) {
+      this.setState({ showSettings: this.props.showSettings }, () => {
+        this.suppressHats();
+      });
+    }
+  }
+
+  suppressHats = settings => {
+    if (this.state.showSettings === true) {
+      mc.off("tap", eventFunction);
+    } else {
+      mc.on("tap", eventFunction);
+    }
   };
 
   componentDidMount() {
     hatDrop = document.getElementById("hatDrop");
-    this.moveHatDrop();
-    let el = document.querySelector("main")
+    hatDropFrame = window.requestAnimationFrame(this.moveHatDrop);
+    let el = document.querySelector("main");
     mc = new Hammer.Manager(el);
     mc.add(new Hammer.Tap());
     eventFunction = () => {
@@ -39,40 +79,9 @@ class HatDrop extends React.Component {
     mc.on("tap", eventFunction);
   }
 
-  componentDidUpdate(prev, curr) {
-    if (this.props.phase !== phase) {
-      phase = this.props.phase;
-      if(phase === 1) {
-        speed = 8
-      }
-      if(phase === 2) {
-        speed = 11;
-      }
-      if(phase === 3) {
-        speed = 14;
-      }
-      if(phase === 4) {
-        speed = 17;
-      }
-    }
-
-    if(this.state.showSettings !== this.props.showSettings) {
-      this.setState({showSettings: this.props.showSettings}, () => {
-        this.suppressHats()
-      })
-    }
-  }
-
-  suppressHats = (settings) => {
-    if(this.state.showSettings ===  true) {
-      mc.off("tap", eventFunction);
-    } else {
-      mc.on("tap", eventFunction);
-    }
-  }
-
   componentWillUnmount() {
-    clearInterval(hatDropInterval);
+    window.clearTimeout(hatDropInterval);
+    window.cancelAnimationFrame(hatDropFrame);
     hatDropLeft = 0;
     hatDrop.style.left = "-60px";
     mc.off("tap", eventFunction);
@@ -83,24 +92,24 @@ class HatDrop extends React.Component {
     const { rightWall, hatDrop, total, showSettings } = this.props;
     return (
       <ImageContext.Consumer>
-      {value => (
-        <div>
-          <div className="rope" />
+        {value => (
+          <div>
+            <div className="rope" />
             <div className="hatDrop" id="hatDrop">
-            <img src={value.bigWheel.default} className="bigWheel" />
-            <img src={value.mount.default} className="mount" />
-            <img src={value.horn.default} className="horn" />
-            <img
-              src={value.smallWheel.default}
-              className="smallWheel smallWheel1"
-            />
-            <img
-              src={value.smallWheel.default}
-              className="smallWheel smallWheel2"
-            />
+              <img src={value.bigWheel.default} className="bigWheel" />
+              <img src={value.mount.default} className="mount" />
+              <img src={value.horn.default} className="horn" />
+              <img
+                src={value.smallWheel.default}
+                className="smallWheel smallWheel1"
+              />
+              <img
+                src={value.smallWheel.default}
+                className="smallWheel smallWheel2"
+              />
+            </div>
           </div>
-        </div>
-      )}
+        )}
       </ImageContext.Consumer>
     );
   }
